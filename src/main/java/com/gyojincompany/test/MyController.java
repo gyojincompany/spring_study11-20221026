9689909f4f999ad5f8da21f9f629945355e95ee4
gyojincompany <luckyred@gmail.com>
2022-10-27 10:16:55 +0900

    ModelAndView 사용 추가

--- src/main/java/com/gyojincompany/test/MyController.java
+++ src/main/java/com/gyojincompany/test/MyController.java
@@ -3,6 +3,7 @@ package com.gyojincompany.test;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class MyController {
@@ -31,6 +32,16 @@ public class MyController {
 		model.addAttribute("name", "홍길동");//String name="홍길동";
 		
 		return "board/view";
+	}
+	
+	@RequestMapping(value = "/reply")
+	public ModelAndView reply() {
+		
+		ModelAndView mv = new ModelAndView();
+		mv.addObject("name", "홍길동");//String name="홍길동";
+		mv.setViewName("board/reply");
+		
+		return mv;
 	}
 
 }
